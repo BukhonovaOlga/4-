b7b190e1d68520efea87361a8ce3c489ef39d43c
BukhonovaOlga <olgakrivosheeva8@gmail.com>
2024-12-06 17:24:37 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+count_of_home_tasks = 12
+print(str(count_of_home_tasks))
+count_of_elapsed_hours = 1.5
+print(str(count_of_elapsed_hours))
+name_of_course = "Python"
+print(name_of_course)
+time_for_one_task = count_of_elapsed_hours / count_of_home_tasks
+print(str(time_for_one_task))
+print("Курс: ", name_of_course, ", всего задач: ", str(count_of_home_tasks), ", затрачено часов: ",
+      str(count_of_elapsed_hours), ", среднее время выполнения ", str(time_for_one_task), " часа.")
